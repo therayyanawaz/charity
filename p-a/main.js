5e71409f56e34e3a15be350ea54f70bfc524a529
Md Rayyan Nawaz <therayyanawaz@gmail.com>
2023-10-12 16:31:24 +0530

    Add files via upload

---
+++ p-a/main.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var qrImg = document.querySelector('.qr-image');
+    qrImg.src = 'https://i.ibb.co/fkFNvSy/IMG-20231012-155542-943.jpg';
+});
